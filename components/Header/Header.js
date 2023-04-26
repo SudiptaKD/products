5ecdb87d6effa5430b34f09ac34070f71e8b2010
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2023-04-27 05:22:21 +0600

    header done

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,68 @@
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge, Box, SvgIcon } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className='h-[57px] bg-gradient-to-r from-[#371c83] via-[#5c1e89] to-[#7f1f8b] p-2 '>
-sssd
-    </div>
+    <Box className='h-[57px] bg-gradient-to-r from-[#371c83] via-[#5c1e89] to-[#7f1f8b] p-2 flex justify-between'>
+      <Box className="flex justify-start">
+        <Image
+        alt="logo"
+        height={100}
+        width={270}
+        src={"/assets/image/logo1.png"}
+        />
+        <Box className="ml-16 text-xl font-medium flex flex-wrap justify-center content-center">
+          <Link href={"/"} className="underline underline-offset-4">
+          Go to Marketpage
+          </Link>
+        </Box>
+      </Box>
+      <Box className="flex justify-between mt-1.5 gap-6">
+         
+        <form className="relative" >
+          <input type="text"  name="search" className="h-7 rounded-md w-96 min-w-full text-black px-2"/>
+          <Box className="absolute top-1 right-2" >
+          <Image
+        alt="search-icon"
+        height={20}
+        width={20}
+        src={"/assets/image/search-icon.png"}
+        />
+          </Box>
+        </form>
+     
+      <Box className="flex justify-between px-2 gap-4">
+      <Badge color="warning" badgeContent={1}>
+        <Box className="h-9 w-9 bg-slate-700 rounded-lg flex flex-wrap content-center justify-center cursor-pointer" >
+        <SvgIcon component={NotificationsOutlinedIcon} inheritViewBox />
+
+
+        </Box>
+        </Badge>
+        <Badge color="warning" badgeContent={1}>
+        <Box className="h-9 w-9 bg-slate-700 rounded-lg flex flex-wrap content-center justify-center cursor-pointer" >
+        <SvgIcon component={ShoppingCartOutlinedIcon} inheritViewBox />
+
+
+        </Box>
+        </Badge>
+        <Box className="h-9 w-9 bg-white rounded-lg flex flex-wrap content-center justify-center cursor-pointer" >
+        <Image
+        alt="search-icon"
+        height={25}
+        width={25}
+        src={"/assets/image/profile-logo.png"}
+        />
+
+
+        </Box>
+      </Box>
+
+      </Box>
+    </Box>
   )
 }
 

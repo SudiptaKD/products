89041e9638cb25923849cf872f149fbb7ab36ab2
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2023-04-28 04:07:13 +0600

    left done

--- components/HomeLeft/HomeLeft.js
+++ components/HomeLeft/HomeLeft.js
@@ -1,8 +1,124 @@
+import {
+    Box
+} from "@mui/material";
+import List from "./List";
 
 const HomeLeft = () => {
   return (
-    <div>HomeLeft</div>
-  )
-}
+    <Box className="p-2">
+<List/>
 
-export default HomeLeft
+<Box className=" ml-4 mt-2 font-semibold text-md rounded-md">Contents</Box>
+<Box className="font-semibold text-sm ml-6 flex flex-col">
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  {" "}VRChat(quest){" "} <Box className="h-3 w-3 rounded-full bg-green-400 mt-1 "></Box>
+</label>
+<label className="flex gap-1"  >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  {" "}VRChat(PCVR) {" "} <Box className="h-3 w-3 rounded-full bg-sky-400 mt-1 "></Box>
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  {" "}Others
+</label>
+</Box>
+<Box className=" ml-4 mt-2  font-semibold text-md rounded-md">Price</Box>
+<Box className="font-semibold text-sm ml-6 flex flex-col">
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  Under $10
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $10 to $20
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $20 to $30
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $30 to $40
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $40 to $50
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $50 to $70
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  $70 & above
+</label>
+</Box>
+<Box className=" ml-4 mt-2 font-semibold text-md rounded-md">Polygon amount</Box>
+<Box className="font-semibold text-sm ml-6 flex flex-col">
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  Under △7,500
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △7,500 to △10,000
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △10,000 to △15,000
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △15,000 to △20,000
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △20,000 to △32,000
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △32,000 to △70,000
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  △70,000 & Above
+</label>
+</Box>
+<Box className=" ml-4 mt-2 font-semibold text-md rounded-md">Auto upload support</Box>
+<Box className="font-semibold text-sm ml-6 flex flex-col">
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  Supported
+</label>
+<label className="flex gap-1" >
+  <input type="checkbox" />
+  <span class="checkmark"></span>
+  Unsupported
+</label>
+
+</Box>
+
+    </Box>
+  );
+};
+
+export default HomeLeft;
---
+++ components/HomeLeft/List.js
@@ -0,0 +1,65 @@
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+} from "@mui/material";
+
+
+const List = () => {
+  return (
+    <>
+    <Box className=" ml-4  font-semibold text-md rounded-md">Category</Box>
+    <Box>
+      <Accordion disableGutters className=" font-semibold text-sm bg-[#FAFAFA] shadow-none ">
+        <AccordionSummary  aria-controls="panel1a-content" id="panel1a-header"
+            sx={{
+                "&.MuiAccordionSummary-root" : {
+                    minHeight : "20px"
+                },
+                ".MuiAccordionSummary-content" : {
+                    margin : "0px"
+                }
+            }}
+        >
+          <Box className="  bg-[#FAFAFA] hover:bg-slate-200 px-2 py-1 rounded-md">Full Avatar</Box>
+        </AccordionSummary>
+        <AccordionDetails className="pl-8" >
+          <Box>
+            <Accordion disableGutters className=" font-semibold text-sm bg-[#FAFAFA] shadow-none ">
+              <AccordionSummary
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                className="p-0  "
+                sx={{
+                    "&.MuiAccordionSummary-root" : {
+                        minHeight : "20px"
+                    },
+                    ".MuiAccordionSummary-content" : {
+                        margin : "0px"
+                    }
+                }}
+              >
+                <Box className="  bg-[#FAFAFA] hover:bg-slate-200 px-2 py-1 rounded-md">Human Based</Box>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box className=" bg-[#FAFAFA]  cursor-pointer hover:bg-slate-200 px-2 py-1 rounded-md	">Male</Box>
+                <Box className=" bg-[#FAFAFA]  cursor-pointer hover:bg-slate-200 px-2 py-1 rounded-md	">Female</Box>
+                <Box className=" bg-[#FAFAFA]  cursor-pointer hover:bg-slate-200 px-2 py-1 rounded-md	">Unisex</Box>
+              </AccordionDetails>
+            </Accordion>
+          </Box>
+
+          <Box className=" bg-[#FAFAFA]  cursor-pointer hover:bg-slate-200 px-2 py-1 rounded-md	">
+            Animal and mythecal creature based
+          </Box>
+          <Box className=" bg-[#FAFAFA]  cursor-pointer hover:bg-slate-200 px-2 py-1 rounded-md	">Robot Based</Box>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
+      <Box className=" ml-4 cursor-pointer bg-[#FAFAFA] hover:bg-slate-200 px-2 py-1 h-8 w-16 font-semibold text-sm rounded-md">Others</Box>
+  </>
+  )
+}
+
+export default List
